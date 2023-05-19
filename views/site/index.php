3ef1edcf2473e087669a272bd2d47dc8e4f939f0
Z1nRes <tv.noob@bk.ru>
2023-05-19 08:18:30 +0300

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Маппул';
+?>
+<div class="site-index">    
+    <div class="body-content">
+        <p>
+            dasdasd
+        </p>
+
+    </div>
+</div>
